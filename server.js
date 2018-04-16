db1c4008f74a3c3590a2aa59b9a08edb24b188b3
Allan Jones <iamallanjones@gmail.com>
2018-04-16 12:59:25 -0500

    Initial Server to Run with Node/ Nodemon

---
+++ server.js
@@ -0,0 +1,11 @@
+
+//Initializing Server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello'));
+
+//deploy to Heroku or Local Port 5000
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
