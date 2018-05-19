880a1aa899241c2076ea47468c952a4899f33c48
Allan Jones <iamallanjones@gmail.com>
2018-05-19 04:54:50 -0500

    Added New User Creation syncing to DB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Require Dependacies
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -8,6 +9,10 @@ const posts = require("./routes/api/posts");
 
 //Initializing Server
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
