f44e29a04bdb1f69ce5b2d9bcda55decfcc8e2ce
Allan Jones <iamallanjones@gmail.com>
2018-05-19 04:12:54 -0500

    Updated server.js and added model folder with User model created

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-
 //Require Dependacies
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 //Initializing Server
 const app = express();
 
 //DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
+
+//Deploy to Heroku or Local Port 5000
+const port = process.env.PORT || 5000;
 
 //Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello There!'));
-
+app.get("/", (req, res) => res.send("Hello There!"));
 
 //Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-
-//deploy to Heroku or Local Port 5000
-const port = process.env.PORT || 5000;
-
+// Start our server so that it can begin listening to client requests.
 app.listen(port, () => console.log(`Server running on port ${port}`));
