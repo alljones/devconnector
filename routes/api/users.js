854c91bd3178f9f580c2a7dfb0b54930d64b166b
Allan Jones <iamallanjones@gmail.com>
2018-06-26 22:47:04 -0500

    JWT Passport Stragety added

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs"); //for PW
 const jwt = require("jasonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport")
 
 //Load User model
 const User = require("../../models/User");
@@ -90,6 +91,16 @@ router.post('/login', (req, res) =>{
     });
 });
 
+// @router  GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', { session: false}), (req, res) => {
+  res.json({
+    id: req.user.id;
+    name: req.user.name;
+    email: req.user.email 
+  });
+});
 
 
 
