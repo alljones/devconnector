91afa51e64cdee2072121b405df43b598bd90e6d
Allan Jones <iamallanjones@gmail.com>
2018-06-30 01:23:09 -0500

    added post like route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -72,7 +72,7 @@ router.post("/", passport.authenticate("jwt", { session: false }),
 router.delete("/", passport.authenticate("jwt", { session: false }),
 (req, res) => {
     Profile.findOne({ user: req.user.id})
-    .then(profile => {
+    .then(post => {
         Post.findById(req.params.id)
         .then(post => {
             //Check for post owner
@@ -85,6 +85,26 @@ router.delete("/", passport.authenticate("jwt", { session: false }),
         .catch(err => res.statut(404).json({nopostsfound: 'No posts found'}));
     })
 });
+
+// @router  POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post("like/:id", passport.authenticate("jwt", { session: false }),
+(req, res) => {
+    Profile.findOne({ user: req.user.id})
+    .then(post => {
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+            return res.status(400).json({ alreadyliked: 'User already liked this post'});
+        }
+        // Add user id to likes array
+        post.likes.unshift({ user: req.user.id });
+
+        post.save().then(post => res.json(post));
+        })
+        .catch(err => res.statut(404).json({nopostsfound: 'No posts found'}));
+    })
+});
+
 
 
 module.exports = router;
