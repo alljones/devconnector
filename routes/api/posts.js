439719d9d0fea28bf5a72a480d3c0c2a67d7ca35
Allan Jones <iamallanjones@gmail.com>
2018-06-30 01:41:28 -0500

    Added Like & Dislike comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -86,24 +86,59 @@ router.delete("/", passport.authenticate("jwt", { session: false }),
     })
 });
 
-// @router  POST api/posts/like/:id
-// @desc    Like post
-// @access  Private
-router.post("like/:id", passport.authenticate("jwt", { session: false }),
-(req, res) => {
-    Profile.findOne({ user: req.user.id})
-    .then(post => {
-        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
-            return res.status(400).json({ alreadyliked: 'User already liked this post'});
-        }
-        // Add user id to likes array
-        post.likes.unshift({ user: req.user.id });
+    // @router  POST api/posts/like/:id
+    // @desc    Like post
+    // @access  Private
+    router.post("/like/:id", passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Profile.findOne({ user: req.user.id}).then(profile => {
+            Post.findById(req.params.id)
+            .then(post => {
+                if(post.likes.filter(like => like.user.toString() === req.user.id)
+            .length > 0
+        ) {
+                return res.status(400).json({ alreadyliked: 'User already liked this post'});
+            }
+            // Add user id to likes array
+            post.likes.unshift({ user: req.user.id });
 
-        post.save().then(post => res.json(post));
-        })
-        .catch(err => res.statut(404).json({nopostsfound: 'No posts found'}));
-    })
-});
+            post.save().then(post => res.json(post));
+            })
+            .catch(err => res.statut(404).json({ nopostsfound: 'No posts found'}));
+            });
+            
+        });
+
+    
+     // @router  POST api/posts/unlike/:id
+    // @desc    Unlike post
+    // @access  Private
+    router.post("/unlike/:id", passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Profile.findOne({ user: req.user.id}).then(profile => {
+            Post.findById(req.params.id)
+            .then(post => {
+                if(post.likes.filter(like => like.user.toString() === req.user.id)
+            .length === 0
+        ) {
+                return res.status(400).json({ notliked: 'You have not yet liked this post'});
+            }
+            // Get the remove index
+            const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+            // Splice out of the array
+            post.likes.splice(removeIndex, 1);
+
+            // Save
+            post.save().then(post => res.json(post));
+            })
+            .catch(err => res.statut(404).json({ nopostsfound: 'No posts found'}));
+            });
+            
+        });
+
 
 
 
