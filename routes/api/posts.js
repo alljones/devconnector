182207d197dff7dc3c43c88e0fc1baaab17698b2
Allan Jones <Allan.Jones@amtrustgroup.com>
2018-07-02 10:13:33 -0500

    Added Comments to post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -82,7 +82,7 @@ router.delete("/", passport.authenticate("jwt", { session: false }),
             // Delete
             post.remove().then(() => res.json({success: true }));
         })
-        .catch(err => res.statut(404).json({nopostsfound: 'No posts found'}));
+        .catch(err => res.status(404).json({nopostsfound: 'No posts found'}));
     })
 });
 
@@ -104,7 +104,7 @@ router.delete("/", passport.authenticate("jwt", { session: false }),
 
             post.save().then(post => res.json(post));
             })
-            .catch(err => res.statut(404).json({ nopostsfound: 'No posts found'}));
+            .catch(err => res.status(404).json({ nopostsfound: 'No posts found'}));
             });
             
         });
@@ -134,7 +134,7 @@ router.delete("/", passport.authenticate("jwt", { session: false }),
             // Save
             post.save().then(post => res.json(post));
             })
-            .catch(err => res.statut(404).json({ nopostsfound: 'No posts found'}));
+            .catch(err => res.status(404).json({ nopostsfound: 'No posts found'}));
             });
             
         });
@@ -144,7 +144,31 @@ router.delete("/", passport.authenticate("jwt", { session: false }),
     // @access   Private
 
     router.post('/comment/:id', passport.authenticate("jwt", { session: false }),(req, res) => {
+        const { errors, isValid } = validatePostInput(req.body);
 
+            // Check Validation
+            if (!isValid) {
+            //Return any errors with 400 status
+            return res.status(400).json(errors);
+            }
+
+
+
+        Post.findById(req.params.id)
+        .then(post => {
+            const newComment = {
+                text: req.body.text,
+                name: req.body.name,
+                avatar: req.body.avatar,
+                user: req.user.id
+            }
+
+            // Add to comments array
+            post.comments.unshift(newComment);
+
+            post.save().then(post => res.json(post));
+            })
+            .catch(err => res.status(404).json({ nopostsfound: 'No posts found'}));
     });
 
 
