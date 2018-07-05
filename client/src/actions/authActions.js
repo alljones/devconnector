45d9779e810c787ceecb48f90085b31370840bed
Allan Jones <Allan.Jones@amtrustgroup.com>
2018-07-05 11:26:33 -0500

    Add registration with redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,16 @@
-import { TEST_DISPATCH } from "./types";
+import axios from "axois";
+import { GET_ERRORS } from "./types";
 
 // Register User
-export const registeruser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+export const registeruser = userData, history => dispatch => {
+  //Connect & post to backend route
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
